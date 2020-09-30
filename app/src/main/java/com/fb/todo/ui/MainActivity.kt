0a2cbeebdd1b02417eb8b582d4fc57b6e1ea1a96
Francisco Barrios <francisco.barrios@ifyoucan.com>
2020-09-30 22:01:01 +0700

    WIP
    - added new amination for fab, not tested

--- app/src/main/java/com/fb/todo/ui/MainActivity.kt
+++ app/src/main/java/com/fb/todo/ui/MainActivity.kt
@@ -3,12 +3,14 @@ package com.fb.todo.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.fb.todo.R
 import com.fb.todo.databinding.ActivityMainBinding
 import com.fb.todo.ui.activity.TaskDetailActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +23,35 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-
         navView.setupWithNavController(navController)
+
+
+        setUpTransition()
         binding.fabNewTask.setOnClickListener {
-            startActivity(Intent(this, TaskDetailActivity::class.java))
+
+            binding.container.startLayoutAnimation()
         }
+    }
+
+    private fun setUpTransition() {
+        binding.container.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                binding.fabNewTask.drawable.setVisible(false, true)
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@MainActivity, TaskDetailActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(
+                p0: MotionLayout?,
+                p1: Int,
+                p2: Boolean,
+                p3: Float
+            ) {
+            }
+        })
     }
 }
